2f0d0d35b210fd847b5e239f4cf9d6b377e26253
Tushar Shekhar <tusharshekhar@Tushars-MacBook-Air.local>
2024-01-23 11:53:44 +0530

    pure&simple

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import SimpleCounterComponent from './component/SimpleCounterComponent'
+import PureCounterComponent from './component/PureCounterComponent'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+        
+      <SimpleCounterComponent/>
+      <PureCounterComponent/>
+    </>
+  )
+}
+
+export default App
